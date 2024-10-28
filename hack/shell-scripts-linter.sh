a5d274832dcc02a3d5a94e42315f79f2f7c07459
Bharath B <bhb@redhat.com>
2024-10-28 16:43:35 +0530

    KONFLUX-191: Add verification scripts

---
+++ hack/shell-scripts-linter.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+verify_script()
+{
+	if ! find . -type f -name '*.sh' '!' -path './cert-manager/*' '!' -path './cert-manager-operator/*' \
+		-printf "[$(date)] -- INFO  -- checking file %p\n" \
+		-exec podman run --rm -v "$PWD:/mnt" docker.io/koalaman/shellcheck:stable '{}' + ; then
+		exit 1
+	fi
+}
+
+##############################################
+###############  MAIN  #######################
+##############################################
+
+verify_script
+
+exit 0
